4af30c90c2527092d9858dfa0ad1ff2fe0dd7141
Warren Spencer <warrenspencer27@gmail.com>
2020-09-11 16:53:38 -0700

    Updates setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='tokex',
     version='2.0',
-    description="Structured String Parsing Library",
+    description="String tokenizing and parsing library",
+
     author="Warren Spencer",
     author_email="warrenspencer27@gmail.com",
-    url="https://github.com/warrenspe/Tokex",
+    url="https://github.com/warrenspe/tokex",
+    keywords=["tokex", "tokenize", "parse"],
 
-    packages=['tokex', 'tokex.utils', 'tokex.tokenizers'],
+    packages=find_packages(),
+
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Text Processing"
+    ]
 )
