dc03fe5f437d73e52d70e1cc44679c7461ce2de0
Warren Spencer <warrenspencer27@gmail.com>
2020-09-11 17:19:35 -0700

    Bump up version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as fd:
 
 setup(
     name='tokex',
-    version='2.0.1',
+    version='2.0.2',
     description="String tokenizing and parsing library",
     long_description=long_description,
     long_description_content_type="text/markdown",
