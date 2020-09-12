eb3370c48c564bea4fab4950f556dc6a0803fc95
Warren Spencer <warrenspencer27@gmail.com>
2020-09-11 17:19:14 -0700

    Update python versions

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md") as fd:
+    long_description=fd.read()
+
 setup(
     name='tokex',
-    version='2.0',
+    version='2.0.1',
     description="String tokenizing and parsing library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 
     author="Warren Spencer",
     author_email="warrenspencer27@gmail.com",
@@ -20,7 +25,9 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Libraries",
         "Topic :: Text Processing"
     ]
